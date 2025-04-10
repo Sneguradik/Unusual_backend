e9f60e3332f8f83e6896bbcb04f77654ffd9deba
Sneguradik <rrtr845@gmail.com>
2025-04-10 14:44:04 +0300

    Modified filters

--- services/Filter.py
+++ services/Filter.py
@@ -14,25 +14,47 @@ class Filter:
     def apply(self, df:pd.DataFrame) -> pd.DataFrame:
         if not self.active : return df
 
-        res_df : pd.DataFrame
-        match self.condition:
-            case "Equals":
-                res_df = df.loc[df[self.name] == self.value]
-            case "Less":
-                res_df = df.loc[df[self.name] < self.value]
-            case "Greater":
-                res_df = df.loc[df[self.name] > self.value]
-            case "LessOrEquals":
-                res_df = df.loc[df[self.name] <= self.value]
-            case "GreaterOrEquals":
-                res_df = df.loc[df[self.name] >= self.value]
-            case _:
-                res_df = df.loc[df[self.name] == self.value]
+        if self.type == "And":
+            res_df : pd.DataFrame
 
-        if self.useTrigger:
-            if "triggers" in res_df.columns: res_df["trigger"] = 1
-            else : res_df["trigger"] +=1
+            match self.condition:
+                case "Equals":
+                    res_df = df.loc[df[self.name] == self.value]
+                case "Less":
+                    res_df = df.loc[df[self.name] < self.value]
+                case "Greater":
+                    res_df = df.loc[df[self.name] > self.value]
+                case "LessOrEquals":
+                    res_df = df.loc[df[self.name] <= self.value]
+                case "GreaterOrEquals":
+                    res_df = df.loc[df[self.name] >= self.value]
+                case _:
+                    res_df = df.loc[df[self.name] == self.value]
 
-        return res_df
+            if self.useTrigger:
+                if "triggers" in res_df.columns: res_df["triggers"] = 1
+                else : res_df["triggers"] +=1
 
+            return res_df
 
+        elif self.type == "Or" and self.useTrigger:
+
+            if "triggers" in df.columns: df["triggers"] = 0
+
+            match self.condition:
+                case "Equals":
+                    df["triggers"] += int(df[self.name] == self.value)
+                case "Less":
+                    df["triggers"] += int(df[self.name] < self.value)
+                case "Greater":
+                    df["triggers"] += int(df[self.name] > self.value)
+                case "LessOrEquals":
+                    df["triggers"] += int(df[self.name] <= self.value)
+                case "GreaterOrEquals":
+                    df["triggers"] += int(df[self.name] >= self.value)
+                case _:
+                    df["triggers"] += int(df[self.name] != self.value)
+
+            return df
+
+        else: return df
